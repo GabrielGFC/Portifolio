69b8f22094ae05a4df8ba3df08564a1191f4e0d1
GabrielGFC <143818850+GabrielGFC@users.noreply.github.com>
2025-06-04 17:25:41 -0300

    pronto?

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig((_ctx): UserConfigExport => ({
   plugins: [react()],
   base: '/Portifolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  }
 }));
