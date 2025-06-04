0ed91e39e6f0e1a66e74d0fa35936a7a4f2f1a53
GabrielGFC <143818850+GabrielGFC@users.noreply.github.com>
2025-06-04 17:03:14 -0300

    5

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type UserConfigExport } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Certifique-se que o base está correto para o GitHub Pages
+// Corrija o base para '/Portifolio/' para garantir que os assets sejam resolvidos corretamente no GitHub Pages
 export default defineConfig((_ctx): UserConfigExport => ({
   plugins: [react()],
   base: '/Portifolio/', // Caminho correto para deploy em subdiretório
