e66211534098bd592382a73dc7d89bcf3d066c14
GabrielGFC <143818850+GabrielGFC@users.noreply.github.com>
2025-06-04 16:49:17 -0300

    ✨ feat: Página

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig, type UserConfigExport } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-// @ts-ignore
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+
 export default defineConfig((_ctx): UserConfigExport => ({
   plugins: [react()],
-  base: '/Portifolio',
+  base: '/Portifolio/', // barra no final garante que os assets sejam resolvidos corretamente
 }));
