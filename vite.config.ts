a7539abbe1c7f4013e26bec40759e32b12a5a7c7
GabrielGFC <143818850+GabrielGFC@users.noreply.github.com>
2025-06-04 17:33:49 -0300

    123

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ export default defineConfig((_ctx): UserConfigExport => ({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true
+    sourcemap: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   }
 }));
